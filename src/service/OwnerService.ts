86c56d7174830e6e52c2f8b465cc9fcd803425e7
AndrewGanzha <ganzha-2002@mail.ru>
2023-07-04 17:00:58 +0300

    Feat: move service settings for individual file

--- src/service/OwnerService.ts
+++ src/service/OwnerService.ts
@@ -5,11 +5,11 @@ const host_url = "https://parseapi.back4app.com";
 const javascript_key = "e3DEAZ8eWuufDJwgrvjO1LkYV2b84y5z8RB03IgU";
 export const dataBase = [];
 
-Parse.initialize(app_id, javascript_key);
-Parse.serverURL = host_url;
-
-export const fetchAllOwners = async () => {
-  const query = new Parse.Query("Owner");
-  const allOwners = await query.find();
-  console.log(allOwners.attributes);
-};
+export default class OwnersService {
+  static async getAll() {
+    Parse.initialize(app_id, javascript_key);
+    Parse.serverURL = host_url;
+    const query = new Parse.Query("Owner");
+    const allOwners = await query.find();
+  }
+}
