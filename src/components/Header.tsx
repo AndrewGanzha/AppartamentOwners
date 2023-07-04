a783326ef505b8a510cb9afb7b6dc5f3d309ecc1
AndrewGanzha <ganzha-2002@mail.ru>
2023-07-04 18:07:40 +0300

    Feat: Edit Header Text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <header className="h-12 w-auto bg-cyan-400 text-center">
-      <h1 className="p-2 text-xl text-white">Список владельцев квартир</h1>
+      <h1 className="p-2 text-white text-xl">Список владельцев квартир</h1>
     </header>
   );
 };
