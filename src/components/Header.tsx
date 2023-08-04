8e3d96a9038493830cf39198be5827b60a7a8ae6
Андрей Ганжа <andrejganza@MacBook-Air-Andrej.local>
2023-08-04 16:43:26 +0300

    Redesign header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,45 @@
+import { Button, Modal } from "@mui/material";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "1px solid grey",
+  boxShadow: 24,
+  p: 4,
+};
+
 export const Header = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
-    <header className="h-20 w-auto bg-cyan-400 text-center item-center">
-      <h1 className="p-2 text-white text-xl block">
-        Список владельцев квартир
-      </h1>
+    <header className="container text-center">
+      <Typography variant="h3">Владельцы квартир</Typography>
+      <div>
+        <Button onClick={handleOpen}>Добавить владельца</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
+      </div>
     </header>
   );
 };
